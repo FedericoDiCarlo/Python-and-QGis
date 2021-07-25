85527d4bfe4dfd6c3996074433a7136bbe1c6978
Federico Di Carlo <86000830+FedericoDiCarlo@users.noreply.github.com>
2021-07-25 17:07:23 -0300

    Update _2_cleansuit.py
    Comentando sobre el setup preliminar.

--- _2_cleansuit.py
+++ _2_cleansuit.py
@@ -1,10 +1,13 @@
 #########################################################################################
 #########################################################################################
-# SETUP PREAMBLE FOR RUNNING STANDALONE SCRIPTS.
-# NOT NECESSARY IF YOU ARE RUNNING THIS INSIDE THE QGIS GUI.
+######### Preliminary setup to use when not running the model within QGis################
+
+# Hay que importar las librerías si no corrés en QGIS. Importante tener en cuenta para buscar built-in functions o methods.
+
 # print('preliminary setup')
 # import sys
 # import os
+# Importing commands in order to run vector layers and shapefiles:
 
 # from qgis.core import (
 #     QgsApplication, 
@@ -17,12 +20,12 @@
 # # See https://gis.stackexchange.com/a/155852/4972 for details about the prefix 
 # QgsApplication.setPrefixPath('C:/OSGeo4W64/apps/qgis', True)
 # qgs = QgsApplication([], False)
-# qgs.initQgis()
-
-# # Add the path to Processing framework  
+# qgs.initQgis()  
 # sys.path.append('C:/OSGeo4W64/apps/qgis/python/plugins')
+# Setting processing framework
 
-# # Import and initialize Processing framework
+# En processing están los algoritmos que usamos para que QGIS haga lo que necesitamos (Fix geometries, Drop fields,...)
+
 # import processing
 # from processing.core.Processing import Processing
 # Processing.initialize()
